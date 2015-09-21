c38620a8b3cc8492d307d83974a0b763c70c6ccc
lionellbriones <llenoil@gmail.com>
2015-09-21 23:48:01 +0800

    Added bower and Auth

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function (mix) {
+    mix.sass('app.scss', 'resources/css');
+
+    mix.styles([
+        'assets/bower/bootstrap/dist/css/bootstrap.min.css',
+        'css/app.css'
+    ], null, 'resources');
+
+    mix.scripts([
+        'bower/jquery/dist/jquery.min.js',
+        'bower/bootstrap/dist/js/bootstrap.min.js'
+    ], null, 'resources/assets');
+
+    mix.version([
+        'public/css/all.css',
+        'public/js/all.js'
+    ]);
 });
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shop App</title>
+    <link rel="stylesheet" href="{{elixir('css/all.css')}}">
+</head>
+<body>
+<div class="content">
+    @yield('content')
+</div>
+
+<script src="{{elixir('js/all.js')}}"></script>
+@yield('footer')
+</body>
+</html>
